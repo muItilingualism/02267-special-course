7f3fa04cea04138edefb597824b3f4f8fd60988f
muItilingualism <sebhalfdan@gmail.com>
2024-08-17 23:38:27 +0200

    Init task_rest2

---
+++ task_rest2/client-test/src/test/java/dtu/example/SimpleDTUPay.java
@@ -0,0 +1,8 @@
+package dtu.example;
+
+public class SimpleDTUPay {
+    
+    public boolean pay(int amout, String customerId, String merchantId) {
+        throw new UnsupportedOperationException();
+    }
+}

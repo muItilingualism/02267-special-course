f687eea413b42484a3161135d42d888f8bdaed79
muItilingualism <sebhalfdan@gmail.com>
2024-08-29 20:30:13 +0200

    Add simple bank account creation/deletion

--- task_rest2/simple-dtu-pay/src/main/java/org/acme/PaymentService.java
+++ task_rest2/simple-dtu-pay/src/main/java/org/acme/PaymentService.java
@@ -2,11 +2,14 @@ package org.acme;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
+import org.acme.model.Account;
 import org.acme.model.Payment;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
 import jakarta.ws.rs.client.Client;
 import jakarta.ws.rs.client.ClientBuilder;
 import jakarta.ws.rs.client.WebTarget;
@@ -19,8 +22,18 @@ public class PaymentService {
     @ConfigProperty(name = "bank.url")
     String bankUrl;
 
+    @Inject
+    BankService bankService;
+
     public void savePayment(Payment payment) {
         payments.add(payment);
+
+        Random random = new Random();
+
+        Account account = new Account(1000, String.valueOf(random.nextInt()), "John", "Doe");
+        
+        String accountId = bankService.createAccount(account);
+        bankService.deleteAccount(accountId);
     }
 
     public List<Payment> getAllPayments() {

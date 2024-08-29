16955235b0f006e53e051dc0222410f75a3c9fcc
muItilingualism <sebhalfdan@gmail.com>
2024-08-29 17:36:06 +0200

    Add bank service to docker-compose yml

--- task_rest2/run_simpledtupay.sh
+++ task_rest2/run_simpledtupay.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 ( cd ./simple-dtu-pay ; mvn clean package )
+( cd ../bank/bank_service ; ./build.sh localhost 8081 )
+
 if docker-compose up --build -d; then
     echo "Container(s) succesfully started"
 else
     echo "docker-compose command failed"
 fi
-
-# run bank_service build and deploy script
-( cd ../bank/bank_service ; ./build_deploy.sh localhost 8081 )
-
--- task_rest2/run_simpledtupay_and_test.sh
+++ task_rest2/run_simpledtupay_and_test.sh
@@ -4,6 +4,7 @@ set -e
 run_simpledtupay() {
     
     (cd simple-dtu-pay ; mvn clean package )
+    ( cd ../bank/bank_service ; ./build.sh localhost 8081 )
 
     if docker-compose up --build -d; then
         echo "SimpleDTUPay service started successfully"

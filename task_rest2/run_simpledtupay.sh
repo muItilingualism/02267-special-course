a042357920f547abcc4daf1352d53eb279858cec
muItilingualism <sebhalfdan@gmail.com>
2024-08-29 16:41:16 +0200

    Perform mvn clean package on deploy
    Otherwise new endpoints wont be detected.

--- task_rest2/run_simpledtupay.sh
+++ task_rest2/run_simpledtupay.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-echo "If you are unable to access the end-points, run 'mvn clean package before this script"
-
+( cd ./simple-dtu-pay ; mvn clean package )
 if docker-compose up --build -d; then
     echo "Container(s) succesfully started"
 else

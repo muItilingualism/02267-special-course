ec9c1803bab410e8942819c11900fd6ab0c4794f
muItilingualism <sebhalfdan@gmail.com>
2024-08-29 17:46:49 +0200

    Add simple bank test endpoint

--- task_rest2/simple-dtu-pay/src/main/java/org/acme/PaymentService.java
+++ task_rest2/simple-dtu-pay/src/main/java/org/acme/PaymentService.java
@@ -6,6 +6,10 @@ import java.util.List;
 import org.acme.model.Payment;
 
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.ws.rs.client.Client;
+import jakarta.ws.rs.client.ClientBuilder;
+import jakarta.ws.rs.client.WebTarget;
+import jakarta.ws.rs.core.Response;
 
 @ApplicationScoped
 public class PaymentService {
@@ -25,5 +29,16 @@ public class PaymentService {
 
     public boolean isValidMerchant(String merchantId) {
         return "mid1".equals(merchantId);
+    }
+
+    public int testBank() {
+        Client client = ClientBuilder.newBuilder().build();
+        WebTarget target = client.target("http://bank:8081/index.html");
+
+        Response response = target.request().get();
+        int statusCode = response.getStatus();
+
+        response.close();
+        return statusCode;
     }
 }

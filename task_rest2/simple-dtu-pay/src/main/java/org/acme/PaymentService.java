8ed67d503b123902032880ad06663c53ab1a8cd5
muItilingualism <sebhalfdan@gmail.com>
2024-08-30 17:05:10 +0200

    Remove PaymentService account creation

--- task_rest2/simple-dtu-pay/src/main/java/org/acme/PaymentService.java
+++ task_rest2/simple-dtu-pay/src/main/java/org/acme/PaymentService.java
@@ -3,22 +3,16 @@ package org.acme;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.acme.model.AccountCreationRequest;
 import org.acme.model.Payment;
 import org.acme.model.PaymentRequest;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
-import jakarta.annotation.PostConstruct;
-import jakarta.annotation.PreDestroy;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 
 @ApplicationScoped
 public class PaymentService {
     private List<PaymentRequest> paymentsRequests = new ArrayList<>();
-    private List<String> accountIds = new ArrayList<>();
-    private AccountCreationRequest customer = new AccountCreationRequest(1000, "cid1", "Cust", "Omer");
-    private AccountCreationRequest merchant = new AccountCreationRequest(1000, "mid1", "Mer", "Chant");
 
     @ConfigProperty(name = "bank.url")
     String bankUrl;
@@ -26,33 +20,9 @@ public class PaymentService {
     @Inject
     BankService bankService;
 
-    @PostConstruct
-    public void postConstruct() {
-        createDefaultAccounts();
-    }
-
-    @PreDestroy
-    public void preDestroy() {
-        deleteDefaultAccounts();
-    }
-
-    private void createDefaultAccounts() {
-        String customerId = bankService.createAccount(customer);
-        String merchantId = bankService.createAccount(merchant);
-
-        accountIds.add(customerId);
-        accountIds.add(merchantId);
-    }
-
-    private void deleteDefaultAccounts() {
-        for (String id : accountIds) {
-            bankService.deleteAccount(id);
-        }
-    }
-
     public void processPayment(PaymentRequest paymentRequest) {
         paymentsRequests.add(paymentRequest);
-        
+
         Payment payment = new Payment(paymentRequest.getAmount(),
                 paymentRequest.getCustomerId(),
                 paymentRequest.getMerchantId(),

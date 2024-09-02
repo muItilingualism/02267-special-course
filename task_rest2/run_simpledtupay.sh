aa886bb4238b51bf52345f80000806f573fa0dd1
muItilingualism <sebhalfdan@gmail.com>
2024-09-02 13:19:55 +0200

    Remove bank rebuild on each run

--- task_rest2/run_simpledtupay.sh
+++ task_rest2/run_simpledtupay.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 ( cd ./simple-dtu-pay ; mvn clean package )
-( cd ../bank/bank_service ; ./build.sh localhost 8081 )
 
 if docker-compose up --build -d; then
     echo "Container(s) succesfully started"

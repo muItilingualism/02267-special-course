e4c1e97a774193c09b7bc8a930de39f48c8e372f
muItilingualism <sebhalfdan@gmail.com>
2024-09-03 14:29:09 +0200

    Rename variables to be more understandable

--- task_rest2/client-test/src/test/java/dtu/example/SimpleDTUPaySteps.java
+++ task_rest2/client-test/src/test/java/dtu/example/SimpleDTUPaySteps.java
@@ -18,32 +18,31 @@ import io.cucumber.java.en.When;
 
 public class SimpleDTUPaySteps {
     String cCpr, mCpr;
-    String cBId, mBId;
+    String cBankId, mBankId;
     SimpleDTUPay dtuPay = new SimpleDTUPay();
     ResponseResult response;
     List<Payment> list;
-    Account cAcc;
-    Account mAcc;
+    Account cBankAccount, mBankAccount;
 
     Bank bank = new Bank();
 
     @Given("a customer with id {string}")
     public void aCustomerWithId(String cpr) {
         this.cCpr = cpr;
-        this.cBId = bank.createAccount(new AccountCreationRequest(1000, cpr, "Cust", "Omer"));
-        cAcc = bank.getAccount(this.cBId);
+        this.cBankId = bank.createAccount(new AccountCreationRequest(1000, cpr, "Cust", "Omer"));
+        cBankAccount = bank.getAccount(this.cBankId);
     }
 
     @Given("a merchant with id {string}")
     public void aMerchantWithId(String cpr) {
         this.mCpr = cpr;
-        this.mBId = bank.createAccount(new AccountCreationRequest(1000, cpr, "Mer", "Chant"));
-        mAcc = bank.getAccount(this.mBId);
+        this.mBankId = bank.createAccount(new AccountCreationRequest(1000, cpr, "Mer", "Chant"));
+        mBankAccount = bank.getAccount(this.mBankId);
     }
 
     @When("the merchant initiates a payment for {int} kr by the customer")
     public void theMerchantInitiatesAPaymentForKrByTheCustomer(int amount) {
-        response = dtuPay.pay(amount, this.cBId, this.mBId);
+        response = dtuPay.pay(amount, this.cBankId, this.mBankId);
     }
 
     @Then("the payment is successful")
@@ -53,23 +52,23 @@ public class SimpleDTUPaySteps {
 
     @Then("the merchant has {int} kr less in his bank account")
     public void theMerchantHasKrLessInHisBankAccount(int amount) {
-        int prevBal = mAcc.getBalance();
-        int currBal = bank.getAccount(this.mBId).getBalance();
+        int prevBal = mBankAccount.getBalance();
+        int currBal = bank.getAccount(this.mBankId).getBalance();
 
         assertTrue((prevBal - amount) == currBal);
     }
 
     @Then("the customer has {int} kr more in his bank account")
     public void theCustomerHasKrMoreInHisBankAccount(int amount) {
-        int prevBal = cAcc.getBalance();
-        int currBal = bank.getAccount(this.cBId).getBalance();
+        int prevBal = cBankAccount.getBalance();
+        int currBal = bank.getAccount(this.cBankId).getBalance();
 
         assertTrue((prevBal + amount) == currBal);
     }
 
     @Given("a successful payment of {int} kr from customer {string} to merchant {string}")
     public void aSuccessfulPaymentOfKrFromCustomerToMerchant(int amount, String customerId, String merchantId) {
-        response = dtuPay.pay(amount, this.cBId, this.mBId);
+        response = dtuPay.pay(amount, this.cBankId, this.mBankId);
         assertTrue(response.isSuccessful());
     }
 
@@ -84,8 +83,8 @@ public class SimpleDTUPaySteps {
         boolean found = false;
         for (Payment payment : this.list) {
             if (payment.getAmount() == amount &&
-                    payment.getCustomerId().equals(cBId) &&
-                    payment.getMerchantId().equals(mBId)) {
+                    payment.getCustomerId().equals(cBankId) &&
+                    payment.getMerchantId().equals(mBankId)) {
                 found = true;
                 break;
             }
@@ -96,12 +95,12 @@ public class SimpleDTUPaySteps {
 
     @Given("an unregistered customer with id {string}")
     public void anUnregisteredCustomerWithId(String cid) {
-        this.cBId = UUID.randomUUID().toString();
+        this.cBankId = UUID.randomUUID().toString();
     }
 
     @Given("an unregistered merchant with id {string}")
     public void anUnregisteredMerchantWithId(String mid) {
-        this.mBId = UUID.randomUUID().toString();
+        this.mBankId = UUID.randomUUID().toString();
     }
 
     @Then("the payment is not successful")
@@ -116,7 +115,7 @@ public class SimpleDTUPaySteps {
 
     @When("the customer is registered")
     public void theAccountIsRegistered() {
-        response = dtuPay.registerCustomer(this.cCpr, this.cBId);
+        response = dtuPay.registerCustomer(this.cCpr, this.cBankId);
     }
 
     @Then("the customer registration is successful")
@@ -126,7 +125,7 @@ public class SimpleDTUPaySteps {
 
     @After
     public void removeBankAccounts() {
-        bank.deleteAccount(cBId);
-        bank.deleteAccount(mBId);
+        bank.deleteAccount(cBankId);
+        bank.deleteAccount(mBankId);
     }
 }

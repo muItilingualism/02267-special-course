f92c5645382bfc14a4c053198073ffb34f7b91d9
muItilingualism <sebhalfdan@gmail.com>
2024-09-03 16:42:22 +0200

    Add UnknownCustomer- and MerchantException handlers

--- task_rest2/simple-dtu-pay/src/main/java/org/acme/PaymentResource.java
+++ task_rest2/simple-dtu-pay/src/main/java/org/acme/PaymentResource.java
@@ -2,6 +2,8 @@ package org.acme;
 
 import org.acme.model.PaymentRequest;
 import org.acme.model.exception.MoneyTransferException;
+import org.acme.model.exception.UnknownCustomerException;
+import org.acme.model.exception.UnknownMerchantException;
 import org.jboss.resteasy.reactive.RestResponse;
 import org.jboss.resteasy.reactive.server.ServerExceptionMapper;
 
@@ -48,9 +50,20 @@ public class PaymentResource {
         return Response.ok(paymentService.getAllPayments()).build();
     }
 
-    //TODO handle specific errors instead of relying on catch-all.
     @ServerExceptionMapper
     public RestResponse<String> mapException(MoneyTransferException x) {
         return RestResponse.status(Response.Status.INTERNAL_SERVER_ERROR, "Failed to transfer money with error " + x.status + " and issue " + x.message);
+    }
+
+    @ServerExceptionMapper
+    public RestResponse<String> mapException(UnknownCustomerException x) {
+        System.out.println("400, customer id is unknown");
+        return RestResponse.status(Response.Status.BAD_REQUEST, "customer id is unknown");
+    }
+
+    @ServerExceptionMapper
+    public RestResponse<String> mapException(UnknownMerchantException x) {
+        System.out.println("400, merchant id is unknon");
+        return RestResponse.status(Response.Status.BAD_REQUEST, "merchant id is unknown");
     }
 }

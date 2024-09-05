bc0b16da8ce5b4f9e3b34a095c40d093118f4845
muItilingualism <sebhalfdan@gmail.com>
2024-09-05 22:55:47 +0200

    Add SimpleDTUPayFacade

---
+++ task_rest2-monolith-event/simple-dtu-pay/src/main/java/org/acme/facade/SimpleDTUPayFacade.java
@@ -0,0 +1,45 @@
+package org.acme.facade;
+
+import java.util.List;
+
+import org.acme.model.AccountRegistrationRequest;
+import org.acme.model.PaymentRequest;
+import org.acme.model.exception.MoneyTransferException;
+import org.acme.model.exception.UnknownCustomerException;
+import org.acme.model.exception.UnknownMerchantException;
+import org.acme.service.account.AccountService;
+import org.acme.service.payment.PaymentService;
+
+import io.smallrye.mutiny.Uni;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.core.Response;
+
+@ApplicationScoped
+public class SimpleDTUPayFacade {
+
+    @Inject
+    PaymentService paymentService;
+
+    @Inject
+    AccountService accountService;
+
+    public void processPayment(PaymentRequest paymentRequest) throws UnknownCustomerException, UnknownMerchantException, MoneyTransferException {
+        paymentService.processPayment(paymentRequest);
+    }
+
+    public List<PaymentRequest> getAllPayments() {
+        return paymentService.getAllPayments();
+    }
+
+    public Uni<Response> processCustomerAccountRegistration(AccountRegistrationRequest account) {
+        return accountService.processCustomerAccountRegistration(account)
+                .onItem().transform(id -> Response.ok(id).build());
+    }
+
+    public Uni<Response> processMerchantAccountRegistration(AccountRegistrationRequest account) {
+        return accountService.processMerchantAccountRegistration(account)
+                .onItem().transform(id -> Response.ok(id).build());
+    }
+    
+}

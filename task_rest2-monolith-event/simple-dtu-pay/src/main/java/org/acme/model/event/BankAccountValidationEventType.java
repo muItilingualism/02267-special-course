9d9932d9ffce4d16abec3e6250966e8c3cfd26a5
muItilingualism <sebhalfdan@gmail.com>
2024-09-05 11:32:16 +0200

    Add BankAccountValidationEventType

---
+++ task_rest2-monolith-event/simple-dtu-pay/src/main/java/org/acme/model/event/BankAccountValidationEventType.java
@@ -0,0 +1,7 @@
+package org.acme.model.event;
+
+public enum BankAccountValidationEventType {
+    BANK_ACCOUNT_VALIDATION_REQUESTED,
+    BANK_ACCOUNT_VALIDATION_SUCCEEDED,
+    BANK_ACCOUNT_VALIDATION_FAILED
+}

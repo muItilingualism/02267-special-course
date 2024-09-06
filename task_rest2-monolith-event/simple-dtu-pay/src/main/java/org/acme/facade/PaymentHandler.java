71b16017813485d8625d34a4e54813e6a70662da
muItilingualism <sebhalfdan@gmail.com>
2024-09-06 21:08:08 +0200

    Add payment event handler

---
+++ task_rest2-monolith-event/simple-dtu-pay/src/main/java/org/acme/facade/PaymentHandler.java
@@ -0,0 +1,68 @@
+package org.acme.facade;
+
+import java.time.Duration;
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeoutException;
+
+import org.acme.model.PaymentRequest;
+import org.acme.model.event.PaymentCompleted;
+import org.acme.model.event.PaymentFailed;
+import org.acme.model.event.PaymentProcessed;
+import org.acme.model.event.PaymentRequested;
+import org.eclipse.microprofile.reactive.messaging.Channel;
+import org.eclipse.microprofile.reactive.messaging.Emitter;
+import org.eclipse.microprofile.reactive.messaging.Incoming;
+
+import io.quarkus.logging.Log;
+import io.smallrye.mutiny.Uni;
+import io.smallrye.reactive.messaging.annotations.Broadcast;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+
+@ApplicationScoped
+public class PaymentHandler {
+
+    @Inject
+    @Channel("payment-requested")
+    @Broadcast
+    Emitter<PaymentRequested> emitter;
+
+    private final ConcurrentHashMap<String, CompletableFuture<Void>> pendingRequests = new ConcurrentHashMap<>();
+
+    public Uni<Void> emit(PaymentRequest request) {
+        String correlationId = UUID.randomUUID().toString();
+        CompletableFuture<Void> future = new CompletableFuture<>();
+        pendingRequests.put(correlationId, future);
+
+        PaymentRequested event = new PaymentRequested(correlationId, request);
+        emitter.send(event);
+        
+        return Uni.createFrom().completionStage(future)
+                .onFailure().invoke(throwable -> {})
+                .ifNoItem().after(Duration.ofSeconds(5)).failWith(
+                        new TimeoutException("Timeout: Payment request took too long"));
+    }
+
+    public CompletableFuture<Void> removeRequest(String correlationId) {
+        return pendingRequests.remove(correlationId);
+    }
+
+    @Incoming("payment-processed")
+    public void handle(PaymentProcessed event) {
+        CompletableFuture<Void> future = removeRequest(event.getCorrelationId());
+        if (future == null) {
+            Log.warn("Received unknown or already removed correlationId: " + event.getCorrelationId());
+        }
+
+        if (event instanceof PaymentCompleted) {
+            future.complete(null);
+        } else if (event instanceof PaymentFailed) {
+            future.completeExceptionally(((PaymentFailed) event).getCause());
+        } else {
+            Log.warnf("Received unhandled PaymentProcess event %s with correlationId: %s",event.getClass(), event.getCorrelationId());
+            future.completeExceptionally(new IllegalStateException("Received unhandled PaymentProcess event"));
+        }
+    }
+}

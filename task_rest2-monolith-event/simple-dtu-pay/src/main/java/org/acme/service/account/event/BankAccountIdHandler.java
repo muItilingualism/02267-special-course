a6449cb8f6dd49fd4ca24aa4ca6abbb081742f53
muItilingualism <sebhalfdan@gmail.com>
2024-09-10 21:59:52 +0200

    Add BankAccountIdHandler

---
+++ task_rest2-monolith-event/simple-dtu-pay/src/main/java/org/acme/service/account/event/BankAccountIdHandler.java
@@ -0,0 +1,27 @@
+package org.acme.service.account.event;
+
+import org.acme.model.event.BankAccountIdAssembled;
+import org.acme.model.event.BankAccountIdRequested;
+import org.acme.service.account.AccountService;
+import org.eclipse.microprofile.reactive.messaging.Incoming;
+import org.eclipse.microprofile.reactive.messaging.Outgoing;
+
+import io.smallrye.reactive.messaging.annotations.Broadcast;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+
+@ApplicationScoped
+public class BankAccountIdHandler {
+    
+    @Inject
+    AccountService accountService;
+
+    @Incoming("bank-account-id-requested")
+    @Broadcast
+    @Outgoing("bank-account-id-assembled")
+    public BankAccountIdAssembled handleBankAccountIdRequested(BankAccountIdRequested event) {
+        String bankAccountId = accountService.getAccountBankId(event.getAccountId()).orElse(null);
+
+        return new BankAccountIdAssembled(event.getCorrelationId(), bankAccountId);
+    }
+}

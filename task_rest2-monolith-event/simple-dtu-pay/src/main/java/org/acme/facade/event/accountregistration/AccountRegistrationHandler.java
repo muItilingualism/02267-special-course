59c72227e0d6a5dc09ea325d154f08087d8622c6
muItilingualism <sebhalfdan@gmail.com>
2024-09-11 13:12:34 +0200

    Move account registration processing logic to separate class

--- task_rest2-monolith-event/simple-dtu-pay/src/main/java/org/acme/facade/event/accountregistration/AccountRegistrationHandler.java
+++ task_rest2-monolith-event/simple-dtu-pay/src/main/java/org/acme/facade/event/accountregistration/AccountRegistrationHandler.java
@@ -1,15 +1,8 @@
 package org.acme.facade.event.accountregistration;
 
-import java.util.concurrent.CompletableFuture;
-
 import org.acme.model.event.AccountRegistrationProcessed;
-import org.acme.model.event.CustomerAccountRegistrationCompleted;
-import org.acme.model.event.CustomerAccountRegistrationFailed;
-import org.acme.model.event.MerchantAccountRegistrationCompleted;
-import org.acme.model.event.MerchantAccountRegistrationFailed;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 
-import io.quarkus.logging.Log;
 import io.smallrye.reactive.messaging.annotations.Merge;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
@@ -18,32 +11,11 @@ import jakarta.inject.Inject;
 public class AccountRegistrationHandler {
 
     @Inject
-    AccountRegistrationEmitter emitter;
+    AccountRegistrationProcessor processor;
 
     @Merge
     @Incoming("account-registration-processed")
     public void handleAccountRegistrationProcessed(AccountRegistrationProcessed event) {
-        CompletableFuture<String> future = this.emitter.remove(event.getCorrelationId());
-        if (future == null) {
-            Log.warn("Received unknown or already removed account-registration-processed correlationId: "
-                    + event.getCorrelationId());
-            return;
-        }
-
-        if (event instanceof CustomerAccountRegistrationCompleted) {
-            future.complete(((CustomerAccountRegistrationCompleted) event).getId());
-        } else if (event instanceof MerchantAccountRegistrationCompleted) {
-            future.complete(((MerchantAccountRegistrationCompleted) event).getId());
-        } else if (event instanceof MerchantAccountRegistrationFailed) {
-            future.completeExceptionally(((MerchantAccountRegistrationFailed) event).getCause());
-        } else if (event instanceof CustomerAccountRegistrationFailed) {
-            future.completeExceptionally(((CustomerAccountRegistrationFailed) event).getCause());
-        } else {
-            Log.warnf("Received unhandled AccountRegistrationProcessed event %s with correlationId: %s", event.getClass(),
-                    event.getCorrelationId());
-            future.completeExceptionally(new IllegalStateException("Received unhandled AccountRegistrationProcessed event"));
-        }
-    }
-
-    
+        this.processor.processAccountRegistrationProcessed(event);
+    }   
 }
---
+++ task_rest2-monolith-event/simple-dtu-pay/src/main/java/org/acme/facade/event/accountregistration/AccountRegistrationProcessor.java
@@ -0,0 +1,43 @@
+package org.acme.facade.event.accountregistration;
+
+import java.util.concurrent.CompletableFuture;
+
+import org.acme.model.event.AccountRegistrationProcessed;
+import org.acme.model.event.CustomerAccountRegistrationCompleted;
+import org.acme.model.event.CustomerAccountRegistrationFailed;
+import org.acme.model.event.MerchantAccountRegistrationCompleted;
+import org.acme.model.event.MerchantAccountRegistrationFailed;
+
+import io.quarkus.logging.Log;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+
+@ApplicationScoped
+public class AccountRegistrationProcessor {
+
+    @Inject
+    AccountRegistrationEmitter emitter;
+
+    public void processAccountRegistrationProcessed(AccountRegistrationProcessed event) {
+        CompletableFuture<String> future = this.emitter.remove(event.getCorrelationId());
+        if (future == null) {
+            Log.warn("Received unknown or already removed account-registration-processed correlationId: "
+                    + event.getCorrelationId());
+            return;
+        }
+
+        if (event instanceof CustomerAccountRegistrationCompleted) {
+            future.complete(((CustomerAccountRegistrationCompleted) event).getId());
+        } else if (event instanceof MerchantAccountRegistrationCompleted) {
+            future.complete(((MerchantAccountRegistrationCompleted) event).getId());
+        } else if (event instanceof MerchantAccountRegistrationFailed) {
+            future.completeExceptionally(((MerchantAccountRegistrationFailed) event).getCause());
+        } else if (event instanceof CustomerAccountRegistrationFailed) {
+            future.completeExceptionally(((CustomerAccountRegistrationFailed) event).getCause());
+        } else {
+            Log.warnf("Received unhandled AccountRegistrationProcessed event %s with correlationId: %s", event.getClass(),
+                    event.getCorrelationId());
+            future.completeExceptionally(new IllegalStateException("Received unhandled AccountRegistrationProcessed event"));
+        }
+    }
+}
